77cc4ac2a13f81358733e60abc093718c9df7ee5
Aniya Allen <aniyaallen@aniyas-macbook-pro.local>
2019-10-20 21:49:14 -0500

    Initial Commit

---
+++ Flixster/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Flixster
+//
+//  Created by Aniya Allen on 10/20/19.
+//  Copyright © 2019 Aniya Allen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
